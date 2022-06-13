25fe2bc4f071d3bf3fee676e4fe63506686a750b
Dsakkos <dmaster456@yahoo.com>
2022-06-13 04:49:02 -0400

    Users CRUD
    Users CRUD Assignment

---
+++ users_crud/server.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect
+# import the class from users.py
+from user import User
+app = Flask(__name__)
+app.secret_key = "coding dojo"
+
+@app.route('/')
+def home():
+    return redirect('/users')
+
+
+@app.route('/users/new')
+def index():
+    return render_template("new_users.html" )
+
+
+@app.route('/users/create', methods=["POST"])
+def create_user():
+    data = {
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"],
+    }
+    User.create(data)
+    return redirect('/users')
+
+
+@app.route('/users/<int:id>')
+def show_user(id):
+    return render_template('show_user.html', user = User. 
+    get_user_by_id({"id": id}))
+
+@app.route('/users')
+def show_users():
+    return render_template('users.html', all_user = User.
+    get_all_users())
+
+
+@app.route('/users/edit/<int:id>')
+def edit_user(id):
+    return render_template('edit_user.html', user = User. 
+    get_user_by_id({"id": id}))
+
+@app.route('/users/update', methods = ['POST'])
+def update_user():
+    data = {
+        "id": request.form['id'],
+        "fname": request.form["fname"],
+        "lname" : request.form["lname"],
+        "email" : request.form["email"],
+    }
+    User.update_user(data)
+    return redirect('/users')
+
+@app.route('/users/delete/<int:id>')
+def delete_user(id):
+    User.delete({"id": id})
+    return redirect('/users')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ users_crud/user.py
@@ -0,0 +1,46 @@
+# import the function that will return an instance of a connection
+from mysqlconnection import connectToMySQL
+# model the class after the users table from our database
+class User:
+    db = "users_schema"
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def create(cls, data):
+        query = "INSERT INTO users ( first_name , last_name , email ) VALUES ( %(fname)s , %(lname)s , %(email)s);"
+        return connectToMySQL(cls.db).query_db( query, data)
+
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL(cls.db).query_db( query)
+        user_list = []
+        for row in results:
+            user_list.append( cls(row))
+        return user_list
+
+
+
+
+    @classmethod
+    def get_user_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id=%(id)s;"
+        results = connectToMySQL(cls.db).query_db( query, data)
+        return cls(results[0])
+
+    @classmethod
+    def update_user(cls, data):
+        query = "UPDATE users SET first_name=%(fname)s, last_name=%(lname)s, email=%(email)s WHERE id=%(id)s"
+        connectToMySQL(cls.db).query_db( query, data )
+        return
+
+    @classmethod
+    def delete(cls, data):
+        query = "DELETE FROM users WHERE id=%(id)s;"
+        connectToMySQL(cls.db).query_db(query,data)
